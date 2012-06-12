ec9b709e1ab9826f9e5e071807bed13e4ee4f14e
AJ Michels <aj.michels@wolfnet.com>
2012-06-12 14:46:01 -0500

    updates to support backward computability

--- com/mlsfinder/wordpress/listing/template/ListingFilmStrip.php
+++ com/mlsfinder/wordpress/listing/template/ListingFilmStrip.php
@@ -12,7 +12,22 @@
  * 
  */
 
-$instanceId = uniqid('mlsFinder_listingFilmStrip_');
+$instanceId	= uniqid( 'mlsFinder_listingFilmStrip_' );
+
+$wait		= 'false';
+if ( is_bool( $options['wait'] ) && $options['wait'] ) {
+	$wait		= 'true';
+}
+
+$waitLen	= 1000;
+if ( is_numeric( $options['waitLen'] ) ) {
+	$waitLen	= $options['waitLen'] * 1000;
+}
+
+$speed		= 40;
+if ( is_numeric( $options['speed'] ) && $options['speed'] != 0 ) {
+	$speed		= round( 10 / ( $options['speed'] / 100 ) );
+}
 
 ?>
 
@@ -22,10 +37,10 @@ $instanceId = uniqid('mlsFinder_listingFilmStrip_');
 
 <script type="text/javascript">
 	
-	jQuery('#<?php echo $instanceId; ?>').mlsFinderFilmStrip({
-		'wait' : <?php echo ($options['wait']) ? 'true' : 'false'; ?>, 
-		'waitLen' : <?php echo $options['waitLen'] * 1000 ; ?>,
-		'speed' : <?php echo round(($options['speed'] != 0) ? 10 / ($options['speed'] / 100) : 40) ; ?>
-	});
+	jQuery( '#<?php echo $instanceId; ?>' ).mlsFinderFilmStrip( {
+		'wait'		: <?php echo $wait; ?>, 
+		'waitLen'	: <?php echo $waitLen; ?>,
+		'speed'		: <?php echo $speed; ?>
+	} );
 	
 </script>
