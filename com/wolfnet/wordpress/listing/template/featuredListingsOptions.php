e82190591b54e51505b8fa662ed80c33edb5806b
AJ Michels <aj.michels@wolfnet.com>
2012-08-22 15:55:21 -0500

    Added input type @21479803

--- com/wolfnet/wordpress/listing/template/featuredListingsOptions.php
+++ com/wolfnet/wordpress/listing/template/featuredListingsOptions.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- * This is an HTML template file for the Listing Film Strip Widget instance Options Form page in the 
+ * This is an HTML template file for the Listing Film Strip Widget instance Options Form page in the
  * WordPress admin. This file should ideally contain very little PHP.
- * 
+ *
  * @package       com.wolfnet.wordpress
  * @subpackage    listing.template
  * @title         featuredListingsOptions.php
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, WolfNet Technologies, LLC
- * 
+ *
  */
 
 ?>
@@ -18,69 +18,69 @@
 
 <div class="wolfnet_featuredListingsOptions">
 
-	<input id="<?php echo $directionId; ?>" name="<?php echo $directionName; ?>" type="hidden" 
+	<input id="<?php echo $directionId; ?>" name="<?php echo $directionName; ?>" type="hidden"
 		class="wolfnet_featuredListingsOptions_dirField" />
-	
+
 	<table class="form-table">
 		<tr>
 			<td><label>Title:</label></td>
-			<td><input id="<?php echo $titleId; ?>" name="<?php echo $titleName; ?>" value="<?php echo $titleValue; ?>" /></td>
+			<td><input id="<?php echo $titleId; ?>" name="<?php echo $titleName; ?>" value="<?php echo $titleValue; ?>" type="text" /></td>
 		</tr>
 		<tr>
 			<td><label for="<?php echo $autoPlayId; ?>">Scroll Control</label></td>
 			<td>
-				<select id="<?php echo $autoPlayId; ?>" name="<?php echo $autoPlayName; ?>" 
+				<select id="<?php echo $autoPlayId; ?>" name="<?php echo $autoPlayName; ?>"
 					class="wolfnet_featuredListingsOptions_autoPlayField">
 					<option value="true"<?php echo $autoPlayTrue; ?>>Automatic & Manual</option>
 					<option value="false"<?php echo $autoPlayFalse; ?>>Manual Only</option>
 				</select>
 				<span class="wolfnet_moreInfo">
 					When set to 'Manual Only' users will only be able to see additional properties
-					by directly interacting with the listings. Controls will be made available on 
+					by directly interacting with the listings. Controls will be made available on
 					either side which when hovered over will advance through the listings.
 				</span>
 			</td>
 		</tr>
-		
+
 		<tr class="wolfnet_featuredListingsOptions_autoPlayOptions">
 			<td colspan="2">
-				
+
 				<fieldset>
-					
+
 					<legend>Automatic Playback Options</legend>
-					
+
 					<table class="form-table">
-						
+
 						<tr>
 							<td><label for="<?php echo $directionId; ?>">Direction:</label></td>
 							<td>
-								<select id="<?php echo $directionId; ?>" name="<?php echo $directionName; ?>" 
+								<select id="<?php echo $directionId; ?>" name="<?php echo $directionName; ?>"
 									class="wolfnet_featuredListingsOptions_autoDirField">
 									<option value="right"<?php echo $autoDirectionRight; ?>>Left to Right</option>
 									<option value="left"<?php echo $autoDirectionLeft; ?>>Right to Left</option>
 								</select>
 							</td>
 						</tr>
-						
+
 						<tr>
 							<td><label for="<?php echo $speedId; ?>">Animation Speed:</label></td>
 							<td>
-								<input id="<?php echo $speedId; ?>" name="<?php echo $speedName; ?>" type="text" 
+								<input id="<?php echo $speedId; ?>" name="<?php echo $speedName; ?>" type="text"
 									value="<?php echo $speedValue; ?>" size="2" maxlength="2" />
 								<span class="wolfnet_moreInfo">
-									Animation speed controls how fast the listings will scroll by 
+									Animation speed controls how fast the listings will scroll by
 									when the scroll control option is set to Automatic.
 								</span>
 							</td>
 						</tr>
-						
+
 					</table>
-					
+
 				</fieldset>
-				
+
 			</td>
 		</tr>
-		
+
 		<tr>
 			<td><label for="<?php echo $ownerTypeId; ?>">Agent/Broker:</label></td>
 			<td>
@@ -92,41 +92,41 @@
 					<?php } ?>
 				</select>
 				<span class="wolfnet_moreInfo">
-					By default as many agent listings are show as possible.  If there are fewer 
-					agent listings than is set for the maximum results the remaining spaces are 
-					filled with broker listings. The other options allow for choosing between only 
+					By default as many agent listings are show as possible.  If there are fewer
+					agent listings than is set for the maximum results the remaining spaces are
+					filled with broker listings. The other options allow for choosing between only
 					displaying broker listings and only agent listings.
 				</span>
 			</td>
 		</tr>
-		
+
 		<tr>
 			<td><label for="<?php echo $maxResultsId; ?>">Max Results:</label></td>
 			<td>
-				<input id="<?php echo $maxResultsId; ?>" name="<?php echo $maxResultsName; ?>" 
+				<input id="<?php echo $maxResultsId; ?>" name="<?php echo $maxResultsName; ?>"
 					value="<?php echo $maxResultsValue; ?>" type="text" size="2" maxlength="2" />
 				<span class="wolfnet_moreInfo">
-					The maximum number of listings which will be displayed. It is possible for there 
-					to be fewer listings displayed than the value of this field. This field is 
+					The maximum number of listings which will be displayed. It is possible for there
+					to be fewer listings displayed than the value of this field. This field is
 					capped at 50.
 				</span>
 			</td>
 		</tr>
-		
+
 	</table>
-	
+
 </div>
 
 <script type="text/javascript">
-	
+
 	if ( typeof jQuery != 'undefined' ) {
-		
+
 		( function ( $ ) {
-			
+
 			$('.wolfnet_featuredListingsOptions').wolfnetFeaturedListingsControls();
-			
+
 		} )( jQuery ); /* END: jQuery IIFE */
-		
+
 	} /* END: If jQuery Exists */
-	
+
 </script>
--- com/wolfnet/wordpress/listing/template/listingGridOptions.php
+++ com/wolfnet/wordpress/listing/template/listingGridOptions.php
@@ -1,29 +1,29 @@
 <?php
 
 /**
- * This is an HTML template file for the Grid Widget instance Options Form page in the 
+ * This is an HTML template file for the Grid Widget instance Options Form page in the
  * WordPress admin. This file should ideally contain very little PHP.
- * 
+ *
  * @package       com.wolfnet.wordpress
  * @subpackage    listing.template
  * @title         listingGridOptions.php
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, WolfNet Technologies, LLC
- * 
+ *
  */
 
 ?>
 
 <div class="wolfnet_listingGridOptions">
-	
+
 	<table class="form-table">
-		
+
 		<tr>
 			<td><label>Title:</label></td>
-			<td><input id="<?php echo $titleId; ?>" name="<?php echo $titleName; ?>" value="<?php echo $titleValue; ?>" /></td>
+			<td><input id="<?php echo $titleId; ?>" name="<?php echo $titleName; ?>" value="<?php echo $titleValue; ?>" type="text" /></td>
 		</tr>
-		
+
 		<tr>
 			<td><label>Price:</label></td>
 			<td>
@@ -46,23 +46,23 @@
 				</select>
 			</td>
 		</tr>
-		
+
 		<tr>
 			<td><label>City:</label></td>
 			<td>
-				<input id="<?php echo $cityId; ?>" name="<?php echo $cityName; ?>" 
+				<input id="<?php echo $cityId; ?>" name="<?php echo $cityName; ?>"
 					type="text" value="<?php echo $cityValue; ?>" />
 			</td>
 		</tr>
-		
+
 		<tr>
 			<td><label>Zipcode:</label></td>
 			<td>
-				<input id="<?php echo $zipcodeId; ?>" name="<?php echo $zipcodeName; ?>" 
+				<input id="<?php echo $zipcodeId; ?>" name="<?php echo $zipcodeName; ?>"
 					type="text" value="<?php echo $zipcodeValue; ?>" />
 			</td>
 		</tr>
-		
+
 		<tr>
 			<td><label>Agent/Broker:</label></td>
 			<td>
@@ -80,20 +80,20 @@
 				</span>
 			</td>
 		</tr>
-		
+
 		<tr>
 			<td><label>Max Results:</label></td>
 			<td>
-				<input id="<?php echo $maxResultsId; ?>" name="<?php echo $maxResultsName; ?>" 
+				<input id="<?php echo $maxResultsId; ?>" name="<?php echo $maxResultsName; ?>"
 					type="text" value="<?php echo $maxResultsValue; ?>" />
 				<span class="wolfnet_moreInfo">
-					The maximum number of listings which will be displayed. It is possible for there 
-					to be fewer listings displayed than the value of this field. This field is 
+					The maximum number of listings which will be displayed. It is possible for there
+					to be fewer listings displayed than the value of this field. This field is
 					capped at 50.
 				</span>
 			</td>
 		</tr>
-		
+
 	</table>
-	
+
 </div>
