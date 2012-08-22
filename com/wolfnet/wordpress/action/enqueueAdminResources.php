41d31a61317ce04018aaa922b4dad3cc9c17383b
AJ Michels <aj.michels@wolfnet.com>
2012-08-22 15:51:09 -0500

    Fixed some issues with css and javascript

--- com/wolfnet/wordpress/action/enqueueAdminResources.php
+++ com/wolfnet/wordpress/action/enqueueAdminResources.php
@@ -3,7 +3,7 @@
 /**
  * This action is responsible for enqueuing any admin resources such as JavaScript and CSS that are
  * needed for any code generated in the WordPress admin for the plugin.
- * 
+ *
  * @package       com.wolfnet.wordpress
  * @subpackage    action
  * @title         enqueueAdminResources.php
@@ -11,68 +11,72 @@
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, WolfNet Technologies, LLC
- * 
+ *
  */
 class com_wolfnet_wordpress_action_enqueueAdminResources
 extends com_ajmichels_wppf_action_action
 {
-	
-	
+
+
 	/* PROPERTIES ******************************************************************************* */
-	
+
 	/**
-	 * This property holds the URL string to the plugin directory. This URL is needed to accurately 
+	 * This property holds the URL string to the plugin directory. This URL is needed to accurately
 	 * define the path to the resource files.
 	 *
 	 * @type  string
-	 * 
+	 *
 	 */
 	private $pluginUrl = '';
-	
-	
+
+
 	/* PUBLIC METHODS *************************************************************************** */
-	
+
 	/**
 	 * This method is executed by the ActionManager when any hooks that this action is registered to
 	 * are encountered.
 	 *
 	 * @return  void
-	 * 
+	 *
 	 */
 	public function execute ()
 	{
 		$this->log( 'Action EnqueueAdminResources' );
 		$url = $this->getPluginUrl();
-		wp_enqueue_script( 'wolfnetadminjs',  $url . 'js/wolfnetAdmin.src.js' );
-		wp_enqueue_style(  'wolfnetadmincss', $url . 'css/wolfnetAdmin.src.css', array(), false, 'screen' );
+
+		wp_enqueue_script( 'tooltipjs',       $url . 'js/jQuery/jquery.tooltip.src.js', array('jquery') );
+		wp_enqueue_script( 'wolfnetadminjs',  $url . 'js/wolfnet.src.js',               array('jquery','tooltipjs') );
+		wp_enqueue_script( 'wolfnetadminjs',  $url . 'js/wolfnetAdmin.src.js',          array('jquery') );
+
+		wp_enqueue_style(  'wolfnetadmincss', $url . 'css/wolfnetAdmin.src.css',        array(), false, 'screen' );
 	}
-	
-	
+
+
 	/* ACCESSOR METHODS ************************************************************************* */
-	
+
 	/**
 	 * GETTER: This method is a getter for the pluginUrl property.
 	 *
 	 * @return  string  The absolute URL to this plugin's directory.
-	 * 
+	 *
 	 */
 	public function getPluginUrl ()
 	{
 		return $this->pluginUrl;
 	}
-	
-	
+
+
 	/**
 	 * SETTER: This method is a setter for the pluginUrl property.
 	 *
 	 * @param   string  $url  The absolute URL to this plugin's directory.
 	 * @return  void
-	 * 
+	 *
 	 */
 	public function setPluginUrl ( $url )
 	{
 		$this->pluginUrl = $url;
 	}
-	
-	
+
+
 }
--- js/wolfnetAdmin.src.js
+++ js/wolfnetAdmin.src.js
@@ -1,47 +1,47 @@
 /**
  * This script is a general container for JavaScript code sepcific to the WordPress admin interface.
- * 
+ *
  * @title         wolfnetAdmin.js
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
- * 
+ *
  */
 
 /**
- * The following code relies on jQuery so if jQuery has been initialized encapsulate the following 
- * code inside an immediately invoked function expression (IIFE) to avoid naming conflicts with the $ 
+ * The following code relies on jQuery so if jQuery has been initialized encapsulate the following
+ * code inside an immediately invoked function expression (IIFE) to avoid naming conflicts with the $
  * variable.
  */
 if ( typeof jQuery != 'undefined' ) {
-	
+
 	( function ( $ ) {
-		
-		/* Make sure there is always a scrollbar on the window so there is no jumping of content 
+
+		/* Make sure there is always a scrollbar on the window so there is no jumping of content
 		 * when sections are expanded and collapsed. */
 		$( 'html' ).css( 'overflow-y', 'scroll' );
-		
+
 		$.fn.wolfnetFeaturedListingsControls = function ( options ) {
-			
+
 			var option = $.extend( {}, options );
-			
+
 			var animationSpeed = 'fast';
 			var easing         = 'swing';
-			
+
 			return this.each( function () {
-				
+
 				var  widgetCtrls = this;
 				var $widgetCtrls = $( this );
-				
+
 				var $playMode   = $widgetCtrls.find( '.wolfnet_featuredListingsOptions_autoPlayField:first' );
 				var $autoFields = $widgetCtrls.find( '.wolfnet_featuredListingsOptions_autoPlayOptions:first' ).hide();
-				
+
 				var showAutoFields = function ()
 				{
 					$autoFields.disabled = false;
 					$autoFields.show();
 					$autoFields.find( 'fieldset:first' ).slideDown( animationSpeed, easing );
 				}
-				
+
 				var hideAutoFields = function ()
 				{
 					$autoFields.val( '' );
@@ -50,9 +50,9 @@ if ( typeof jQuery != 'undefined' ) {
 						$autoFields.hide();
 					} );
 				}
-				
+
 				$playMode.change( function () {
-					
+
 					/* Automatic */
 					if ( $( this ).val() == 'true' ) {
 						showAutoFields();
@@ -61,15 +61,15 @@ if ( typeof jQuery != 'undefined' ) {
 					else {
 						hideAutoFields();
 					}
-					
+
 				} );
-				
+
 				$playMode.trigger( 'change' );
-				
+
 			} );
-			
+
 		}
-		
+
 	} )( jQuery ); /* END: jQuery IIFE */
-	
+
 } /* END: If jQuery Exists */
