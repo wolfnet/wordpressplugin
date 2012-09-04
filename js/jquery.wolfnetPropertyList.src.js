5464daa6fa342cce18f76671ba14e77ef143a3eb
AJ Michels <aj.michels@wolfnet.com>
2012-09-04 09:46:08 -0500

    Fixed javascript for property list widgets

--- js/jquery.wolfnetPropertyList.src.js
+++ js/jquery.wolfnetPropertyList.src.js
@@ -1,72 +1,84 @@
 /**
- * This jQuery plugin can be applied to any number of containers which hold child elements which 
+ * This jQuery plugin can be applied to any number of containers which hold child elements which
  * will then be displayed in a list format.
- * 
+ *
  * @title         jquery.wolfnetPropertyList.src.js
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
- * 
+ *
  */
 
 /* Make sure the 'trim' function is available in the String object. Fix for older versions of IE. */
 if(typeof String.prototype.trim !== 'function') {
 	String.prototype.trim = function() {
-		return this.replace(/^\s+|\s+$/g, ''); 
+		return this.replace(/^\s+|\s+$/g, '');
 	}
 }
 
 /**
- * The following code relies on jQuery so if jQuery has been initialized encapsulate the following 
- * code inside an immediately invoked function expression (IIFE) to avoid naming conflicts with the $ 
+ * The following code relies on jQuery so if jQuery has been initialized encapsulate the following
+ * code inside an immediately invoked function expression (IIFE) to avoid naming conflicts with the $
  * variable.
  */
 if ( typeof jQuery != 'undefined' ) {
-	
+
 	( function ( $ ) {
-		
+
 		$.fn.wolfnetPropertyList = function ( options ) {
-			
+
 			var option = $.extend( {}, options );
-			
+
 			return this.each( function () {
-				
+
 				var $this      = $( this );
-				var $listWidth = $this.find( 'ul:first' ).width();
-				
-				$this.find( '.wolfnet_listing li' ).css( 'white-space', 'nowrap' );
-				$this.find( '.wolfnet_listing div' ).css( 'white-space', 'nowrap' );
-				$this.find( '.wolfnet_listing a' ).css( 'white-space', 'nowrap' );
-				$this.find( '.wolfnet_listing span' ).css( 'white-space', 'nowrap' );
-				$this.find( '.wolfnet_listing span' ).css( 'float', 'none' );
-				
-				var $listItemWidth = $this.find( 'li a' ).width();
-				
-				if ( $listItemWidth > $listWidth ) {
-					$this.find( 'li span.full_address' ).each( function () {
-						
-						var $this = $( this );
-						var $a = $this.parent( 'a' );
-						var content = $this.html();
-						var contentLen = content.length;
-						var trimNum = 1;
-						
-						while ( $a.width() > $listWidth ) {
-							
-							$this.html( content.substring( 0, ( contentLen - trimNum ) ).trim() + '... ' );
-							
-							trimNum++;
-							
-						}
-						
-					} );
-				}
-				
-				$this.find( '.wolfnet_listing span.price' ).css( 'float', 'right' );
-				
+				var listWidth  = $this.width();
+				var $items     = $this.find( 'div.wolfnet_listing' );
+
+				console.log( listWidth );
+
+				$this.css( {
+					'position' : 'absolute'
+				} );
+
+				$items.find( '*:visible' ).css( {
+					'white-space' : 'nowrap',
+					'display'     : 'inline',
+					'float'       : 'none',
+					'overflow'    : 'visible'
+				} );
+
+				$items.each( function () {
+
+					var $item = $( this );
+					var $link = $item.find( 'a:first' );
+					var $addr = $item.find( '.full_address:first' );
+					var cont  = $addr.html();
+					var len   = cont.length;
+					var trim  = 1;
+
+					console.log( $link, $link, $link.width() );
+
+					while ( $link.width() > listWidth ) {
+
+						$addr.html( cont.substring( 0, ( len - trim ) ).trim() + '... ' );
+						trim++;
+
+					}
+
+				} );
+
+				$this.css( {
+					'position' : 'static'
+				} );
+
+				$this.find( '.wolfnet_listing span.price' ).css( {
+					'float' : 'right'
+				} );
+
 			} ); /* END: for each loop of elements the plugin has been applied to. */
-			
+
 		}; /* END: function $.fn.wolfnetPropertyList */
-		
+
 	} )( jQuery ); /* END: jQuery IIFE */
-	
+
 } /* END: If jQuery Exists */
