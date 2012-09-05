c1728037ad114f990a843c0790c4e75f8c7a8fd5
AJ Michels <aj.michels@wolfnet.com>
2012-09-05 10:05:50 -0500

    Adding placeholder content for support page. @21479805

--- com/wolfnet/wordpress/admin/template/pluginInstructions.php
+++ com/wolfnet/wordpress/admin/template/pluginInstructions.php
@@ -1,23 +1,27 @@
 <?php
 
 /**
- * This is an HTML template file for the Plugin Settings page in the WordPress admin. This file 
+ * This is an HTML template file for the Plugin Settings page in the WordPress admin. This file
  * should ideally contain very little PHP.
- * 
+ *
  * @package       com.wolfnet.wordpress
  * @subpackage    admin.template
  * @title         pluginInstructions.php
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, WolfNet Technologies, LLC
- * 
+ *
  */
 
 ?>
 <div class="wrap">
-	
-	<div id="icon-options-wolfnet" class="icon32"><br></div>
-	
+
+	<div id="icon-options-wolfnet" class="icon32"><br/></div>
+
 	<h2>WolfNet - Plugin Support</h2>
-	
+
+	<p>For questions about the plugin please contact WolfNet Technologies customer services at
+		612-342-0088 or toll free at 1-866-WOLFNET. You may also reach us online via our website
+		<a href="http://wolfnet.com/contact_us.cfm" target="_blank">WolfNet.com</a>.</p>
+
 </div>
