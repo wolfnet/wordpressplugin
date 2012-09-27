7d31e243e666e16504aace69e21e9707f451241d
AJ Michels <aj.michels@wolfnet.com>
2012-09-27 15:06:09 -0500

    Some minor text update. #4-6 @21484805

--- com/wolfnet/wordpress/admin/template/searchManager.php
+++ com/wolfnet/wordpress/admin/template/searchManager.php
@@ -10,17 +10,17 @@
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, WolfNet Technologies, LLC
- *                
+ *
  *                This program is free software; you can redistribute it and/or
  *                modify it under the terms of the GNU General Public License
  *                as published by the Free Software Foundation; either version 2
  *                of the License, or (at your option) any later version.
- *                
+ *
  *                This program is distributed in the hope that it will be useful,
  *                but WITHOUT ANY WARRANTY; without even the implied warranty of
  *                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *                GNU General Public License for more details.
- *                
+ *
  *                You should have received a copy of the GNU General Public License
  *                along with this program; if not, write to the Free Software
  *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -44,7 +44,7 @@
 
 		<p>The <strong>Search Manager</strong> allows you to create and save custom searches for use
 			in shortcodes and widgets. The WordPress Search Manager works much the same way as the
-			URL Search Builder within the MLS Finder Admin.</p>
+			URL Search Builder within the MLSFinder Admin.</p>
 
 		<p>Custom searches can target any of the search criteria that is available on your property
 			search. Keep in mind that some search criteria is more restrictive than others, which
--- com/wolfnet/wordpress/listing/template/listingGridOptions.php
+++ com/wolfnet/wordpress/listing/template/listingGridOptions.php
@@ -10,17 +10,17 @@
  * @contributors  AJ Michels (aj.michels@wolfnet.com)
  * @version       1.0
  * @copyright     Copyright (c) 2012, WolfNet Technologies, LLC
- *                
+ *
  *                This program is free software; you can redistribute it and/or
  *                modify it under the terms of the GNU General Public License
  *                as published by the Free Software Foundation; either version 2
  *                of the License, or (at your option) any later version.
- *                
+ *
  *                This program is distributed in the hope that it will be useful,
  *                but WITHOUT ANY WARRANTY; without even the implied warranty of
  *                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *                GNU General Public License for more details.
- *                
+ *
  *                You should have received a copy of the GNU General Public License
  *                along with this program; if not, write to the Free Software
  *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -139,7 +139,7 @@
 					type="text" value="<?php echo $maxResultsValue; ?>" />
 				<span class="wolfnet_moreInfo">
 					Define the number of properties to be included in a search results set.
-					he maximum number of properties that can be displayed is 50.
+					The maximum number of properties that can be displayed is 50.
 				</span>
 			</td>
 		</tr>
