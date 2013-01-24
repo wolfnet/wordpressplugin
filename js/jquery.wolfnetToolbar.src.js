9af03fa11cd5ca5cd7a383d357e0f48792f1f05e
andrew.baumgart <andrew.baumgart@wolfnet.com>
2013-01-24 08:09:13 -0600

    @21503137 Toolbar jquery plugin for pagination tools

--- js/jquery.wolfnetToolbar.src.js
+++ js/jquery.wolfnetToolbar.src.js
@@ -17,11 +17,11 @@ if ( typeof jQuery != 'undefined' ) {
 
 			var title = getTitleHeader(titleString);
 
-			var toolbar = renderResultsToolbar( usesPagination,resultsPerPage );
+			var toolbar = renderSortDropdown( usesPagination,resultsPerPage );
 			$( this ).prepend( toolbar.clone() ).append( toolbar.clone() );
 
 			if (usesPagination == 'true') {				
-				var pagination = renderPaginationToolbar( usesPagination,resultsPerPage );
+				var pagination = renderSortByDropdown( resultsPerPage );
 				$( this ).prepend( pagination.clone() ).append( pagination.clone() );
 			}
 
@@ -29,25 +29,23 @@ if ( typeof jQuery != 'undefined' ) {
 
 		} /*END: function $.fn.wolfnetToolbar*/
 
+
 		var getTitleHeader = function ( titleString ) {
 			var header = $('<h2>').text(titleString);
 			return header;
 		}
 
+
 		// Method to build out results toolbar
-		var renderResultsToolbar = function ( usesPagination
+		var renderSortDropdown = function ( usesPagination
 											, resultsPerPage ) {
 
-			var resultTools = $('<div>').addClass('toolbar_div').css( {'width':'100%','clear':'both'} );
+			var resultTools = $('<div>').addClass('sort_div').css( {'width':'100%','clear':'both'} );
 
 			// Horizontal cells within toolbar div
 			var cells = [];
-			cells[1] = $('<div>').appendTo(resultTools).css( 
-				{'width':'33%','float':'left','test-align':'left'} );
-			cells[2] = $('<div>').appendTo(resultTools).css( 
-				{'width':'33%','float':'left','text-align':'center'} );
-			cells[3] = $('<div>').appendTo(resultTools).css( 
-				{'width':'33%','float':'right','text-align':'right'} ); 
+			cells[0] = $('<div>').appendTo(resultTools).css( 
+				{'width':'99%','clear':'both','text-align':'left'} );
 
 			//Build Sort By dropdown and append to first cell
 			var sortByDropdown = $('<select>').addClass( 'sortoptions' );
@@ -55,7 +53,7 @@ if ( typeof jQuery != 'undefined' ) {
 				url: '?pagename=wolfnet-listing-sortoptions',
 				dataType: 'json',
 				success: function ( data ) {
-					var select = $( '.toolbar_div' ).find( 'select.sortoptions' );
+					var select = $( '.sort_div' ).find( 'select.sortoptions' );
 					select.empty();
 					for ( var key in data ) {
 						$("<option>", {value:data[key][0],text:data[key][1]}).appendTo( select );
@@ -63,11 +61,16 @@ if ( typeof jQuery != 'undefined' ) {
 					}
 				}
 			});
-			$(sortByDropdown).appendTo(cells[1]);
+			$(sortByDropdown).appendTo(cells[0]);
 
-			//Build results preview string and append to second cell
-			var resultsCount = getResultsCountString(usesPagination,resultsPerPage);
-			$(cells[2]).text(resultsCount);
+			return resultTools;
+		}
+
+
+		// Method to build out results toolbar
+		var renderSortByDropdown = function ( resultsPerPage ) {
+
+			var paginationToolbar = $('<div>').addClass('pagination_div').css( {'width':'100%','clear':'both'} );;
 
 			//Build show # of listings dropdown and append to third cell
 			var showDropdown = $('<select>').addClass( 'showlistings' );
@@ -75,42 +78,66 @@ if ( typeof jQuery != 'undefined' ) {
 				url: '?pagename=wolfnet-listing-showlistings',
 				dataType: 'json',
 				success: function ( data ) {
-					var select = $( '.toolbar_div' ).find( 'select.showlistings' );
+					var select = $( '.pagination_div' ).find( 'select.showlistings' );
 					select.empty();
 					for ( var key in data ) {
 						$("<option>", {value:data[key],text:data[key]}).appendTo( select );
 					}
+					$("<option>", {value:resultsPerPage,text:resultsPerPage}).appendTo(select).attr("selected","selected");;
 				}
 			});			
 			var showPerPage = $(showDropdown).before('Show').after('per page');
+
+			// Horizontal cells within toolbar div
+			var cells = [];
+			cells[0] = $('<div>').appendTo(paginationToolbar).css( 
+				{'width':'33%','float':'left','test-align':'left'} );
+			cells[1] = $('<div>').appendTo(paginationToolbar).css( 
+				{'width':'33%','float':'left','text-align':'center'} );
+			cells[2] = $('<div>').appendTo(paginationToolbar).css( 
+				{'width':'33%','float':'right','text-align':'right'} ); 
+
+			//cell to store Show # dropdown
+			cells[3] = $('<div>').appendTo(paginationToolbar).css( 
+				{'width':'99%','clear':'both','text-align':'center'} ); 
+
+			//Build results preview string and append to second cell
+			var resultsCount = getResultsCountString(resultsPerPage);
+			$(cells[1]).text(resultsCount);
+
 			$(showPerPage).appendTo(cells[3]);
 
-			return resultTools;
-		}
-
-
-		// Method to build out results toolbar
-		var renderPaginationToolbar = function ( usesPagination
-											   , resultsPerPage ) {
-
-			var paginationToolbar = $('<div>').addClass('pagination_div').css( {'width':'100%','clear':'both'} );;
-
-			var cells = [];
-			cells[1] = $('<div>').appendTo(paginationToolbar).css( 
-				{'width':'50%','float':'left','test-align':'left'} );
-			cells[2] = $('<div>').appendTo(paginationToolbar).css( 
-				{'width':'50%','float':'right','text-align':'right'} );  
-
-			cells[1].text('<<Previous');
+			cells[0].text('<<Previous');
 			cells[2].text('Next>>');
 
+/*
+			$("<a>").appendTo(cells[0]).addClass("previousPage").text("<<Previous").attr("href","?pagename=wolfnet-get-previous-results");
+			$("<a>").appendTo(cells[2]).addClass("nextPage").text("Next>>").attr("href","?pagename=wolfnet-get-next-results");;
+
+			$('.previousPage').click(function(e){
+				e.preventDefault();
+				var valueToPass = $(this).text();
+				var url = "?pagename=wolfnet-get-previous-results";
+				$.post(url, { data: valueToPass }, function( data ){
+
+				} );
+			});
+
+			$('.nextPage').click(function(e){
+				e.preventDefault();
+				var valueToPass = $(this).text();
+				var url = "?pagename=wolfnet-get-next-results";
+				$.post(url, { data: valueToPass }, function( data ){
+
+				} );
+			});
+*/
 			return paginationToolbar;
 		}
 
+
 		// Method to calculate and return results preview string
-		var getResultsCountString = function ( usesPagination
-											 , resultsPerPage ) {
-			if (usesPagination == 'false')
+		var getResultsCountString = function ( resultsPerPage ) {
 			var preview = 'Results 1-' + resultsPerPage + ' of XXX';
 			return preview;
 		}
