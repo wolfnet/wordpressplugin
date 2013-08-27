28ff838463eeb4799d7d3520ebd1288ad13a89b3
AJ Michels <aj.michels@wolfnet.com>
2013-08-27 11:48:37 -0500

    Fixing bug with special characters in search parameters. @21574315

--- wolfnet.php
+++ wolfnet.php
@@ -1511,7 +1511,7 @@ class wolfnet
 
         foreach ($_GET as $param => $paramValue) {
             if (!array_search($param, $resParams)) {
-                $paramValue = urlencode(htmlspecialchars_decode($paramValue));
+                $paramValue = urlencode($this->html_entity_decode_numeric($paramValue));
                 $url .= "&{$param}={$paramValue}";
             }
         }
@@ -1612,7 +1612,7 @@ class wolfnet
             }
 
             if ($valid) {
-                $url .= '&' . $key . '=' . urlencode(htmlspecialchars_decode($value));
+                $url .= '&' . $key . '=' . urlencode($this->html_entity_decode_numeric($value));
             }
 
         }
@@ -2294,6 +2294,62 @@ class wolfnet
         foreach ($ajxActions as $action => $method) {
             $this->addAction('wp_ajax_' . $action, array(&$this, $method));
         }
+
+    }
+
+
+    /**
+    * Decodes all HTML entities, including numeric and hexadecimal ones.
+    *
+    * @param mixed $string
+    * @return string decoded HTML
+    */
+    function html_entity_decode_numeric($string, $quote_style=ENT_COMPAT, $charset='utf-8')
+    {
+        $string = html_entity_decode($string, $quote_style, $charset);
+        $string = preg_replace_callback('~&#x([0-9a-fA-F]+);~i', array($this, 'chr_utf8_callback'), $string);
+        $string = preg_replace('~&#([0-9]+);~e', 'chr_utf8("\\1")', $string);
+
+        return $string;
+
+    }
+
+
+    /**
+     * Callback helper
+     */
+    public function chr_utf8_callback($matches)
+    {
+        return chr_utf8(hexdec($matches[1]));
+
+    }
+
+
+    /**
+    * Multi-byte chr(): Will turn a numeric argument into a UTF-8 string.
+    *
+    * @param mixed $num
+    * @return string
+    */
+    private function chr_utf8($num)
+    {
+        if ($num < 128) {
+            return chr($num);
+        }
+
+        if ($num < 2048) {
+            return chr(($num >> 6) + 192) . chr(($num & 63) + 128);
+        }
+
+        if ($num < 65536) {
+            return chr(($num >> 12) + 224) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);
+        }
+
+        if ($num < 2097152) {
+            return chr(($num >> 18) + 240) . chr((($num >> 12) & 63) + 128) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);
+        }
+
+        return '';
 
     }
 
