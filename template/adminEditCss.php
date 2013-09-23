657b6194dc1aafe7eb276785ded47f647b481d30
michael.pidde@wolfnet.com <michael.pidde@wolfnet.com>
2013-09-23 14:58:57 -0500

    @21583443 - Shuffling layout and adding instructionary text.

--- template/adminEditCss.php
+++ template/adminEditCss.php
@@ -4,20 +4,33 @@
 
     <h2>WolfNet - Edit CSS</h2>
 
+    <p>This is a high-level tool that should only be used by a web developer or designer who is familiar with CSS.</p>
+
     <form method="post" action="options.php">
 
     	<?php echo $formHeader; ?>
 
     	<fieldset>
 
-    		<legend><h3>Edit CSS</h3></legend>
-
             <table class="form-table">
 
             	<tr>
-            		<th scope="col"><label for="wolfnetCss_adminCss">Admin CSS</label></th>
-            		<th scope="col"><label for="wolfnetCss_publicCss">Public CSS</label></th>
+            		<th class="wolfnet_cssLabel"><label for="wolfnetCss_publicCss">Public CSS</label></th>
             	</tr>
+
+                <tr>
+                    <td>
+                        <?php /* <div id="wolfnet_publicCss" class="wolfnet_cssBox">
+                            <?php echo $publicCss; ?>
+                        </div> */ ?>
+                        <p class="wolfnet_note">This CSS will affect the public-facing pages of your web site.</p>
+                        <textarea id="wolfnetCss_publicCss" name="wolfnetCss_publicCss" class="wolfnet_cssBox"><?php echo $publicCss; ?></textarea>
+                    </td>
+                </td>
+
+                <tr>
+                    <th class="wolfnet_cssLabel"><label for="wolfnetCss_adminCss">Admin CSS</label></th>
+                </tr>
 
             	<tr>
             		<td>
@@ -27,17 +40,10 @@
                         <p class="wolfnet_note">This CSS will affect your Wordpress admin (the pages you are looking at now).</p>
             			<textarea id="wolfnetCss_adminCss" name="wolfnetCss_adminCss" class="wolfnet_cssBox"><?php echo $adminCss; ?></textarea>
             		</td>
-            		<td>
-            			<?php /* <div id="wolfnet_publicCss" class="wolfnet_cssBox">
-            				<?php echo $publicCss; ?>
-            			</div> */ ?>
-                        <p class="wolfnet_note">This CSS will affect the public-facing pages of your web site.</p>
-            			<textarea id="wolfnetCss_publicCss" name="wolfnetCss_publicCss" class="wolfnet_cssBox"><?php echo $publicCss; ?></textarea>
-            		</td>
             	</tr>
 
             	<tr>
-            		<td colspan="2" class="submit">
+            		<td class="submit">
             			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
             		</td>
             	</tr>
