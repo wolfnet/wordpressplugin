04558dff43c0909f070419b769fadfbe5031a0a8
Andrew Baumgart <andrew.baumgart@wolfnet.com>
2014-03-27 14:27:44 -0500

    @21629340 null lat/lng were possibly causing some sporadic issues in the mapquest API when mapquest was assigning "NaN" to null numbers - not adding a listing to houseover array if lat/lng are null since it won't be appearing on map anyway

--- wolfnet.php
+++ wolfnet.php
@@ -2282,51 +2282,55 @@ class wolfnet
 
         foreach ($listingsData as $listing) {
 
-            $concatHouseover  = '<a style="display:block" rel="follow" href="' . $listing->property_url . '">';
-            $concatHouseover .= '<div class="wolfnet_wntHouseOverWrapper">';
-            $concatHouseover .= '<div data-property-id="' . $listing->property_id . '" class="wntHOItem">';
-            $concatHouseover .= '<table class="wolfnet_wntHOTable">';
-            $concatHouseover .= '<tbody>';
-            $concatHouseover .= '<tr>';
-            $concatHouseover .= '<td class="wntHOImgCol" valign="top" style="vertical-align:top;">';
-            $concatHouseover .= '<div class="wolfnet_wntHOImg">';
-            $concatHouseover .= '<img src="' . $listing->thumbnail_url . '" style="max-height:100px;width:auto">';
-            $concatHouseover .= '</div>';
-            if ($showBrokerImage) {
-                $concatHouseover .= '<div class="wolfnet_wntHOBroker" style="text-align: center">';
-                $concatHouseover .= '<img class="wolfnet_wntHOBrokerLogo" src="' . $listing->branding->brokerLogo . '" alt="Broker Reciprocity">';
-                $concatHouseover .= '</div>';
-            }
-            $concatHouseover .= '</td>';
-            $concatHouseover .= '<td valign="top" style="vertical-align:top;">';
-            $concatHouseover .= '<div class="wolfnet_wntHOContentContainer">';
-            $concatHouseover .= '<div style="text-align:left;font-weight:bold">' . $listing->listing_price;
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '<div style="text-align:left;">' . $listing->display_address;
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '<div style="text-align:left;">' . $listing->city . ', ' . $listing->state;
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '<div style="text-align:left;">' . $listing->bedsbaths;
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '<div style="text-align:left;padding-top:20px;">' . $listing->branding->content;
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '</td>';
-            $concatHouseover .= '</tr>';
-            $concatHouseover .= '</tbody>';
-            $concatHouseover .= '</table>';
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '</div>';
-            $concatHouseover .= '</a>';
+            if (!is_null($listing->lat) && !is_null($listing->lng)) {
 
-            array_push($houseoverData, array(
-                'lat'        => $listing->lat,
-                'lng'        => $listing->lng,
-                'content'    => $concatHouseover,
-                'propertyId' => $listing->property_id,
-                'propertyUrl'=> $listing->property_url
-                ));
-        }
+                $concatHouseover  = '<a style="display:block" rel="follow" href="' . $listing->property_url . '">';
+                $concatHouseover .= '<div class="wolfnet_wntHouseOverWrapper">';
+                $concatHouseover .= '<div data-property-id="' . $listing->property_id . '" class="wntHOItem">';
+                $concatHouseover .= '<table class="wolfnet_wntHOTable">';
+                $concatHouseover .= '<tbody>';
+                $concatHouseover .= '<tr>';
+                $concatHouseover .= '<td class="wntHOImgCol" valign="top" style="vertical-align:top;">';
+                $concatHouseover .= '<div class="wolfnet_wntHOImg">';
+                $concatHouseover .= '<img src="' . $listing->thumbnail_url . '" style="max-height:100px;width:auto">';
+                $concatHouseover .= '</div>';
+                if ($showBrokerImage) {
+                    $concatHouseover .= '<div class="wolfnet_wntHOBroker" style="text-align: center">';
+                    $concatHouseover .= '<img class="wolfnet_wntHOBrokerLogo" src="' . $listing->branding->brokerLogo . '" alt="Broker Reciprocity">';
+                    $concatHouseover .= '</div>';
+                }
+                $concatHouseover .= '</td>';
+                $concatHouseover .= '<td valign="top" style="vertical-align:top;">';
+                $concatHouseover .= '<div class="wolfnet_wntHOContentContainer">';
+                $concatHouseover .= '<div style="text-align:left;font-weight:bold">' . $listing->listing_price;
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '<div style="text-align:left;">' . $listing->display_address;
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '<div style="text-align:left;">' . $listing->city . ', ' . $listing->state;
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '<div style="text-align:left;">' . $listing->bedsbaths;
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '<div style="text-align:left;padding-top:20px;">' . $listing->branding->content;
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '</td>';
+                $concatHouseover .= '</tr>';
+                $concatHouseover .= '</tbody>';
+                $concatHouseover .= '</table>';
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '</div>';
+                $concatHouseover .= '</a>';
+    
+                array_push($houseoverData, array(
+                    'lat'        => $listing->lat,
+                    'lng'        => $listing->lng,
+                    'content'    => $concatHouseover,
+                    'propertyId' => $listing->property_id,
+                    'propertyUrl'=> $listing->property_url
+                    ));
+
+            }
+        }  
 
         return $houseoverData;
 
