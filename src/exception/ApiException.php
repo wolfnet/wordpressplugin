3ef9557515e04038a36a32ed3648354b399e1b4f
AJ Michels <ajmichels.web@gmail.com>
2015-03-03 15:48:17 -0600

    Adding comments and documentation.

--- src/exception/ApiException.php
+++ src/exception/ApiException.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * API Exceptions
+ *
+ * This exception class is use by the API Client to express any exceptions that occur during
+ * the process of communicating wit the WolfNet API.
+ *
+ * This exception does not currently have any state or functionality of its own but exists as an
+ * extension of the Wolfnet_Exception class so that we can more easily catch exception of this
+ * specific type.
+ *
+ * @package Wolfnet
+ * @copyright 2015 WolfNet Technologies, LLC.
+ * @license GNU v2
+ *
+ */
 class Wolfnet_Exception_ApiException extends Wolfnet_Exception
 {
 
--- src/exception/Exception.php
+++ src/exception/Exception.php
@@ -6,16 +6,44 @@ class Wolfnet_Exception extends Exception
 
     /* PROPERTIES ******************************************************************************* */
 
+    /**
+     * This property is used to hold details about the exception which we do not want to share as
+     * openly as the standard exception message.
+     *
+     * @var string
+     *
+     */
     protected $details = '';
 
+    /**
+     * This property holds any data that is related to the exception and may be useful for debugging.
+     * @var mixed
+     */
     protected $data = null;
 
+    /**
+     * This property serves as a back fill for older versions of PHP which do not include the
+     * previous exception support. This property holds a reference to any previous exception that
+     * was thrown before the current exception.
+     * @var Exception
+     */
     protected $previous = null;
 
 
     /* CONSTRUCTOR ****************************************************************************** */
 
-    public function __construct($message, $details='', $data=null, Exception $previous=null, $code=0)
+    /**
+     * This constructor argument collects our unique exception data as well as standard exception
+     * data. This constructor calls the parent constructor function to utilize it's infrastructure
+     * where appropriate.
+     *
+     * @param string     $message   A basic message string.
+     * @param string     $details   A more detailed exception string (not as public, but not private).
+     * @param mixed      $data      Arbitrary data that may be useful for debugging.
+     * @param Exception  $previous  An exception that was throw before the current instance.
+     * @param mixed      $code      An arbitrary code to make error handling easier.
+     */
+    public function __construct($message, $details='', $data=null, Exception &$previous=null, $code=0)
     {
         parent::__construct($message, $code);
 
@@ -28,12 +56,24 @@ class Wolfnet_Exception extends Exception
 
     /* PUBLIC METHODS *************************************************************************** */
 
+    /**
+     * This method is used to retrieve the details string.
+     * @return string
+     */
     public function getDetails()
     {
         return $this->details;
     }
 
 
+    /**
+     * This method retrieves any data that was included with the exception. If no data was found in
+     * the current exception it attempts to retrieve any data included with the previous exception
+     * if one exists.
+     *
+     * @return mixed
+     *
+     */
     public function getData()
     {
         $previous = $this->getPrevious();
@@ -47,12 +87,29 @@ class Wolfnet_Exception extends Exception
     }
 
 
+    /**
+     * This method gets the previous exception included with the current instance.
+     *
+     * NOTE: This method is getPrev rather that getPrevious because in newer version of PHP
+     * getPrevious is a "final" signature method.
+     *
+     * @return Exception
+     *
+     */
     public function getPrev()
     {
         return $this->previous;
     }
 
 
+    /**
+     * This method can be used to append a string to the exception message.
+     *
+     * @param  string  $message  A message string
+     *
+     * @return Wolfnet_Exception  Current instance (for method chaining)
+     *
+     */
     public function append($message)
     {
         $this->message .= ' ' . $message;
@@ -62,18 +119,20 @@ class Wolfnet_Exception extends Exception
     }
 
 
+    /**
+     * This method can be used to append a string to the exception details.
+     *
+     * @param  string  $details  A details string
+     *
+     * @return Wolfnet_Exception  Current instance (for method chaining)
+     *
+     */
     public function appendDetails($details)
     {
         $this->details .= ' ' . $details;
 
         return $this;
 
-    }
-
-
-    public function __toString()
-    {
-        return parent::__toString() . "\n" . json_encode($this->getData());
     }
 
 
