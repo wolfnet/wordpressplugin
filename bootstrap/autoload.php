e4ae72bd43238fd12156e7e84fc799d630eb4e94
AJ Michels <ajmichels.web@gmail.com>
2015-03-04 09:32:17 -0600

    Using a constant instead of standard variable.

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -4,10 +4,10 @@
  * This file is responsible for auto-loading all WolfNet plugin classes.
  */
 
-$wntClassRoot = dirname(dirname(__FILE__)) . '/src';
+define('WNT_CLASS_ROOT', dirname(dirname(__FILE__)) . '/src');
 
-include $wntClassRoot . '/Autoloader.php';
+include WNT_CLASS_ROOT . '/Autoloader.php';
 
-$autoloader = new Wolfnet_Autoloader($wntClassRoot, include dirname(__FILE__) . '/classmap.php');
+$wntAutoloader = new Wolfnet_Autoloader(WNT_CLASS_ROOT, include dirname(__FILE__) . '/classmap.php');
 
-spl_autoload_register(array($autoloader, 'load'));
+spl_autoload_register(array($wntAutoloader, 'load'));
