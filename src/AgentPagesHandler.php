f6e6586f5f154468dcdb9b41ad7a8538d52c6ad9
Michael <michael.pidde@gmail.com>
2016-01-26 14:34:57 -0600

    chore: Remove debug stuff
    no task

--- src/AgentPagesHandler.php
+++ src/AgentPagesHandler.php
@@ -139,9 +139,6 @@ class Wolfnet_AgentPagesHandler extends Wolfnet_Plugin
         }
 
         try {
-            var_dump($this->key);
-            var_dump($endpoint);
-            var_dump($this->args['criteria']);
             $data = $GLOBALS['wolfnet']->apin->sendRequest($this->key, $endpoint, 'GET', $this->args['criteria']);
         } catch (Wolfnet_Exception $e) {
             return $GLOBALS['wolfnet']->displayException($e);
