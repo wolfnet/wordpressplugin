5bfec5d495bce893f15de6d27253ccc151d58404
John Coleman <thenewcoke@gmail.com>
2016-02-01 21:56:40 +0000

    style: Code formatting
    @21947619

--- src/template/listingHouseover.php
+++ src/template/listingHouseover.php
@@ -21,43 +21,50 @@
 
 
 // this template renders the markup for map property info that shows up on mouse hover.
-if (!is_null($listing['geo']['lat']) && !is_null($listing['geo']['lng'])) : 
+if (!is_null($listing['geo']['lat']) && !is_null($listing['geo']['lng'])) :
     ?>
-    <a style="display:block" rel="follow" href="<?php echo $listing['property_url']; ?>">
-    <div class="wolfnet_wntHouseOverWrapper">
-    <div data-property-id="<?php echo $listing['property_id'] ?>" class="wntHOItem">
-    <table class="wolfnet_wntHOTable">
-    <tbody>
-    <tr>
-    <td class="wntHOImgCol" valign="top" style="vertical-align:top;">
-    <div class="wolfnet_wntHOImg">
-    <img src="<?php echo $listing['thumbnail_url']; ?>" style="max-height:100px;width:auto">
-    </div>
-    <?php if ($showBrokerImage) : ?>
-        <div class="wolfnet_wntHOBroker" style="text-align: center">
-        <img src="<?php echo $listing['branding']['logo']; ?>" style="max-height:50px;width:auto" alt="Broker Reciprocity">
+    <a style="display: block" rel="follow" href="<?php echo $listing['property_url']; ?>">
+        <div class="wolfnet_wntHouseOverWrapper">
+            <div data-property-id="<?php echo $listing['property_id'] ?>" class="wntHOItem">
+                <table class="wolfnet_wntHOTable">
+                    <tbody>
+                        <tr>
+                            <td class="wntHOImgCol" valign="top" style="vertical-align:top;">
+                                <div class="wolfnet_wntHOImg">
+                                    <img src="<?php echo $listing['thumbnail_url']; ?>"
+                                     style="max-height: 100px; width: auto" />
+                                </div>
+                                <?php if ($showBrokerImage) : ?>
+                                    <div class="wolfnet_wntHOBroker" style="text-align: center">
+                                        <img src="<?php echo $listing['branding']['logo']; ?>"
+                                         style="max-height: 50px; width: auto" alt="Broker Reciprocity" />
+                                    </div>
+                                <?php endif; ?>
+                            </td>
+                            <td valign="top" style="vertical-align: top;">
+                                <div class="wolfnet_wntHOContentContainer">
+                                    <div style="text-align: left; font-weight: bold">
+                                        <?php echo $listing['listing_price']; ?>
+                                    </div>
+                                    <div style="text-align: left;">
+                                        <?php echo $listing['display_address']; ?>
+                                    </div>
+                                    <div style="text-align: left;">
+                                        <?php echo $listing['city']; ?>, <?php echo $listing['state']; ?>
+                                    </div>
+                                    <div style="text-align: left;">
+                                        <?php echo $listing['bedsbaths']; ?>
+                                    </div>
+                                    <div style="text-align: left; padding-top: 20px;">
+                                        <?php echo $listing['branding']['courtesy_text']; ?>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    <?php endif; ?>
-    </td>
-    <td valign="top" style="vertical-align:top;">
-    <div class="wolfnet_wntHOContentContainer">
-    <div style="text-align:left;font-weight:bold"><?php echo $listing['listing_price']; ?>
-    </div>
-    <div style="text-align:left;"><?php echo $listing['display_address']; ?>
-    </div>
-    <div style="text-align:left;"><?php echo $listing['city']; ?>, <?php echo $listing['state']; ?>
-    </div>
-    <div style="text-align:left;"><?php echo $listing['bedsbaths']; ?>
-    </div>
-    <div style="text-align:left;padding-top:20px;"><?php echo $listing['branding']['courtesy_text']; ?>
-    </div>
-    </div>
-    </td>
-    </tr>
-    </tbody>
-    </table>
-    </div>
-    </div>
     </a>
 <?php endif; ?>
 
