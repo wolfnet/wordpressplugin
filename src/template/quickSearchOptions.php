10dcb80c275ee66b229c0ab26f3b261a44a5e25a
Andrew Baumgart <andrew.baumgart@wolfnet.com>
2016-02-08 15:22:21 -0600

    feat: Adding hint text copy
    @21951917

--- src/template/quickSearchOptions.php
+++ src/template/quickSearchOptions.php
@@ -61,6 +61,7 @@
 					<option value="0" selected>Disabled</option>
 				</select>
 				<span class="wolfnet_moreInfo">
+					Enabling SmartSearch on your WolfNet Quick Search will allow website visitors to select a specific search type from a drop-down of suggestions based on what they type into the search. The SmartSearch will present the search types available for the criteria they enter, for example: city, area, neighborhood or school district, thus allowing them to further customize their results.
 				</span>
 			</td>
 		</tr>
