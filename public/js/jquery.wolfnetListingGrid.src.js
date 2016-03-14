85b6d8b70c325c8d06bdf38daffa2e38fa9f6c4f
John Coleman <thenewcoke@gmail.com>
2016-03-14 17:52:20 +0100

    refactor: Replaced JS grid resizing with CSS
    @21967565

--- public/js/jquery.wolfnetListingGrid.src.js
+++ public/js/jquery.wolfnetListingGrid.src.js
@@ -66,139 +66,95 @@ if (typeof jQuery !== 'undefined') {
                 data.$container = $target.find('.' + data.option.containerClass);
             }
 
-            var $items = getGridItems(target);
-            // Capture the original item width for later comparison, removing max-width, margins, and padding
-            data.$container.css({
-                'padding-left':  '',
-                'padding-right': ''
-            });
-            resizeItems($items);
-            var itemPadding = data.itemPadding || 0;
-            var itemWidth = $items.first().innerWidth();
-            if (!isNaN(itemPadding)) {
-                itemWidth = Math.max(itemWidth - itemPadding, 0);
-            }
-            data.itemWidth = itemWidth;
-
         };
 
-        var prepareDomElements = function(target)
+
+        var updateColumnWidths = function(target, size)
         {
+            size = size || '';
+
             var $target = $(target);
             var data = $target.data(pluginName);
             var $items = getGridItems(target);
 
-            if (data.option.appendClearfix) {
-                $('<div>').addClass(data.option.clearfixClass).insertAfter($items.last());
+            // Resize items
+            $items.removeClass('wolfnet_listing_sm wolfnet_listing_xs');
+            switch (size) {
+                case 'sm':
+                    $items.addClass('wolfnet_listing_sm');
+                    break;
+                case 'xs':
+                    $items.addClass('wolfnet_listing_xs');
+                    break;
             }
+            $items.trigger('wntResizeItem');
 
-        };
+            // Remove first/last column identifiers
+            $items.removeClass('wolfnet_colFirst wolfnet_colLast');
 
-        var updateColumnWidths = function(target)
-        {
-            var $target = $(target);
-            var data = $target.data(pluginName);
-            var $items = getGridItems(target);
-            var targetWidth = data.$container.innerWidth();
-            var minColumnWidth = data.itemWidth;
-            var columnWidth = minColumnWidth;
-            var columns = Math.max(Math.floor(targetWidth / (columnWidth + data.option.minColumnGap)), 1);
-            var gridAlign = data.option.gridAlign;
-
-            if (columns > $items.length) {
-
-                // If number of columns exceeds number of items, limit columns to number of items
-                columns = $items.length;
-
-            } else if (columns === 1) {
-
-                // If only 1 column can fit, try reducing column width to fit 2 columns
-                var reducedColumnWidth = (targetWidth - data.option.minColumnGap) / 2;
-                if (reducedColumnWidth >= columnWidth * 0.7) {
-                    columns = 2;
-                    columnWidth = reducedColumnWidth;
-                }
-
-            }
-
-            var remainingPixels = targetWidth - (columnWidth * columns);
-            var margin = remainingPixels / (columns + 1);
-            var itemMargin   = columns === 1 ? Math.floor(margin) : 0;
-            var sidePadding  = columns === 1 ? 0 : Math.floor(margin / 2);
-
-            data.itemPadding = sidePadding * 2;
-
-            resizeItems($items, {
-                width:   columnWidth + data.itemPadding,
-                padding: sidePadding,
-                margin:  (gridAlign === 'center' ? itemMargin : 15),
-                align:   gridAlign
-            });
-
-            // Pad the container to center the grid as a whole
-            var containerPadding = Math.floor(
-                (data.$container.innerWidth() - ($items.first().outerWidth(true) * columns)) / 2
-            );
-            data.$container.css({
-                'padding-left':  containerPadding,
-                'padding-right': containerPadding
-            });
-
-            $items.trigger('wntResizeItem')
-                .removeClass('wolfnet_colFirst wolfnet_colLast');
+            // Find the row breaks, and count the columns
+            var $lastItem = null,
+                columns = 0,
+                rows = 0
+                rowItems = 0;
 
             for (var i=0, l=$items.length; i<l; i++) {
 
                 var $item = $($items[i]);
 
-                if (((i + 1) == columns) || ((i + 1) % columns === 0)) {
-                    $item.addClass('wolfnet_colLast');
-                    if ((i + 1) < l) {
-                        var $nextItem = $($items[i + 1]);
-                        $nextItem.addClass('wolfnet_colFirst');
+                if ($lastItem) {
+                    if ($lastItem.offset().top != $item.offset().top) {
+                        $lastItem.addClass('wolfnet_colLast');
+                        $item.addClass('wolfnet_colFirst');
+                        rows++;
                     }
-                }
-
-                if (i === 0) {
+                } else {
                     $item.addClass('wolfnet_colFirst');
+                    rows++;
                 }
 
+                // Count the items in the row
+                rowItems++;
+
+                // In the first row, count the columns
+                if (rows === 1) {
+                    columns = rowItems;
+                }
+
+                // Note the last item
                 if (i == (l - 1)) {
                     $item.addClass('wolfnet_colLast');
                 }
 
+                $lastItem = $item;
+
             }
 
-            data.$container.trigger('columns-updated.' + pluginName);
-
-        };
-
-        var resizeItems = function($items, options)
-        {
-            var marginLeft = '', marginRight = '';
-
-            options = $.extend({}, {
-                width:   '',
-                padding: '',
-                margin:  '',
-                align:   ''
-            }, options || {});
-
-            if (options.align === 'center') {
-                marginLeft = options.margin;
+            if (columns === 1) {
+                // Try resizing to get more columns
+                switch (size) {
+                    case 'full':
+                        // Done
+                        data.$container.trigger('columns-updated.' + pluginName);
+                        break;
+                    case 'xs':
+                        // Go back to full-size, 1-column
+                        updateColumnWidths(target, 'full');
+                        break;
+                    case 'sm':
+                        // Try the next size down
+                        updateColumnWidths(target, 'xs');
+                        break;
+                    default:
+                        // Try the next size down
+                        updateColumnWidths(target, 'sm');
+                        break;
+                }
             } else {
-                marginRight = options.margin;
+                // Done
+                data.$container.trigger('columns-updated.' + pluginName);
             }
 
-            $items.css({
-                'margin-left':   marginLeft,
-                'margin-right':  marginRight,
-                'padding-left':  options.padding,
-                'max-width':     options.width,
-                'width':         (options.width === '' ? 'initial' : '')
-            }).find('.wolfnet_listingMain').css({
-                'padding-right': options.padding
-            });
         };
 
 
@@ -236,27 +192,6 @@ if (typeof jQuery !== 'undefined') {
 
         };
 
-        var updateRowHeight = function(target)
-        {
-            var $target = $(target);
-            var data = $target.data(pluginName);
-            var $items = getGridItems(target);
-            var maxItemHeight = 0;
-            $items.height('auto');
-
-            // Loop over each item to determine what the height of the tallest one is.
-            $items.each(function(){
-                var itemHeight = this.scrollHeight;
-                maxItemHeight = (maxItemHeight < itemHeight) ? itemHeight : maxItemHeight;
-            });
-
-            // Set all items to the same height as the tallest.
-            $items.height(maxItemHeight);
-            $items.css('marginBottom', data.option.minRowGap);
-
-            $target.trigger('rows-updated.' + pluginName);
-
-        };
 
         /* Methods available to the plugin. */
         var methods = {
@@ -328,7 +263,6 @@ if (typeof jQuery !== 'undefined') {
 
                     data.imagesLoading = true;
 
-                    prepareDomElements(target);
                     updateColumnWidths(target);
                     monitorImages(target);
 
@@ -346,13 +280,10 @@ if (typeof jQuery !== 'undefined') {
                     $(target).trigger('refresh-start.' + pluginName);
 
                     preparePluginData(target);
-                    prepareDomElements(target);
                     updateColumnWidths(target);
 
                     if (deep) {
                         monitorImages(target);
-                    } else {
-                        updateRowHeight(target);
                     }
 
                     $(target).trigger('refresh-end.' + pluginName);
