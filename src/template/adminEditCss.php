166643640b479bd34516ee3acbbced168f4210b0
John Coleman <thenewcoke@gmail.com>
2016-03-18 17:09:18 +0100

    feat: Format Admin Edit CSS page

--- src/template/adminEditCss.php
+++ src/template/adminEditCss.php
@@ -12,29 +12,29 @@
 
         <fieldset>
 
-            <table class="form-table">
+            <legend>
+                <h2 class="title">
+                    <label for="wolfnetCss_publicCss">Public CSS</label>
+                </h2>
+            </legend>
 
-                <tr>
-                    <th class="wolfnet_cssLabel"><label for="wolfnetCss_publicCss">Public CSS</label></th>
-                </tr>
+            <p class="description">This CSS will affect the public-facing pages of your web site.</p>
 
-                <tr>
-                    <td>
-                        <p class="wolfnet_note">This CSS will affect the public-facing pages of your web site.</p>
-                        <textarea id="wolfnetCss_publicCss" name="wolfnetCss_publicCss" class="wolfnet_cssBox"><?php echo $publicCss; ?></textarea>
-                    </td>
-                </td>
+            <textarea id="wolfnetCss_publicCss" name="wolfnetCss_publicCss" class="wolfnet_cssBox"><?php echo $publicCss; ?></textarea>
 
-                <tr>
-                    <th class="wolfnet_cssLabel"><label for="wolfnetCss_adminCss">Admin CSS</label></th>
-                </tr>
+        </fieldset>
 
-                <tr>
-                    <td>
-                        <p class="wolfnet_note">This CSS will affect your Wordpress admin (the pages you are looking at now).</p>
-                        <textarea id="wolfnetCss_adminCss" name="wolfnetCss_adminCss" class="wolfnet_cssBox"><?php echo $adminCss; ?></textarea>
-                    </td>
-                </tr>
+        <fieldset>
+
+            <legend>
+                <h2 class="title">
+                    <label for="wolfnetCss_adminCss">Admin CSS</label>
+                </h2>
+            </legend>
+
+            <p class="description">This CSS will affect your Wordpress admin (the pages you are looking at now).</p>
+
+            <textarea id="wolfnetCss_adminCss" name="wolfnetCss_adminCss" class="wolfnet_cssBox"><?php echo $adminCss; ?></textarea>
 
             </table>
 
