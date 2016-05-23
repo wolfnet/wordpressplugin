f31652e88bfbccaa9249b53f6521ff8bc9efb53a
John Coleman <thenewcoke@gmail.com>
2016-05-23 20:43:52 +0100

    feat: Agent/Office grid layouts
    @22002714

--- src/template/agentPagesListAgents.php
+++ src/template/agentPagesListAgents.php
@@ -144,72 +144,93 @@ if (!function_exists('paginate')) {
 
 					<div class="wolfnet_aoItem">
 
-						<a href="<?php echo $agentLink; ?>">
-							<div class="wolfnet_aoImage"
-							 style="background-image: url('<?php echo $agent['medium_url']; ?>');">
-								<img src="<?php echo $agent['medium_url']; ?>"
-								 onerror="this.className += ' wnt-hidden';" />
-							</div>
-						</a>
+						<div class="wolfnet_aoBody">
 
-						<div class="wolfnet_aoInfo">
 
-							<div class="wolfnet_aoContact">
+							<a href="<?php echo $agentLink; ?>">
+								<div class="wolfnet_aoImage"
+								 style="background-image: url('<?php echo $agent['medium_url']; ?>');">
+									<img src="<?php echo $agent['medium_url']; ?>"
+									 onerror="this.className += ' wnt-hidden';" />
+								</div>
+							</a>
 
-								<div class="wolfnet_aoTitle">
+							<div class="wolfnet_aoInfo">
+
+								<div class="wolfnet_aoContact">
+
+									<div class="wolfnet_aoTitle">
+										<?php
+											echo '<a href="' . $agentLink . '">';
+											echo $agent['first_name'] . ' ' . $agent['last_name'];
+											echo '</a>';
+										?>
+									</div>
+
+									<hr />
+
+									<div class="wolfnet_aoSubTitle">
+										<?php $agent['business_name']; ?>
+									</div>
+
+								</div>
+
+								<ul class="wolfnet_aoLinks">
+
 									<?php
-										echo '<a href="' . $agentLink . '">';
-										echo $agent['first_name'] . ' ' . $agent['last_name'];
-										echo '</a>';
+
+										if (strlen($agent['office_phone_number']) > 0) {
+											echo '<li><span class="wnt-icon wnt-icon-phone"></span> '
+												. '<span class="wnt-visuallyhidden">Office phone:</span> '
+												. $agent['office_phone_number'] . '</li>';
+										}
+
+										if (strlen($agent['mobile_phone']) > 0) {
+											echo '<li><span class="wnt-icon wnt-icon-mobile"></span> '
+												. '<span class="wnt-visuallyhidden">Mobile phone:</span> '
+												. $agent['mobile_phone'] . '</li>';
+										}
+
+										if (strlen($agent['email_address']) > 0) {
+											echo '<li><span class="wnt-icon wnt-icon-mail"></span> '
+												. '<span class="wnt-visuallyhidden">Contact:</span> '
+												. '<a href="' . $contactLink . '">'
+												. $agent['first_name'] . ' ' . $agent['last_name']
+												. '</a></li>';
+										}
+
+										if (strlen($agent['address_1']) > 0) {
+											echo '<li><span class="wnt-icon wnt-icon-location"></span> '
+												. '<span class="wnt-visuallyhidden">Address:</span> '
+												. $agent['address_1'] . ' ' . $agent['address_2']
+												. '<br />'
+												. $agent['city'] . ', ' . $agent['state'] . ' '
+												. $agent ['zip_code'];
+										}
+
 									?>
-								</div>
 
-								<hr />
+								</ul>
 
-								<div class="wolfnet_aoSubTitle">
-									<?php $agent['business_name']; ?>
+								<div class="wolfnet_aoActions wolfnet_clearfix">
+									<div class="wolfnet_aoAction">
+										<a class="wnt-btn wnt-btn-secondary"
+										 href="<?php echo $agentLink; ?>">View Profile</a>
+									</div>
+									<?php if (strlen($agent['web_url']) > 0) { ?>
+										<div class="wolfnet_aoAction">
+											<a class="wnt-btn wnt-btn-primary" target="_blank"
+											 href="<?php echo $agent['web_url']; ?>">View Website</a>
+										</div>
+									<?php } ?>
 								</div>
 
 							</div>
 
-							<ul class="wolfnet_aoLinks">
+						</div>
 
-								<?php
-
-									if (strlen($agent['office_phone_number']) > 0) {
-										echo '<li><span class="wnt-icon wnt-icon-phone"></span> '
-											. '<span class="wnt-visuallyhidden">Office phone:</span> '
-											. $agent['office_phone_number'] . '</li>';
-									}
-
-									if (strlen($agent['mobile_phone']) > 0) {
-										echo '<li><span class="wnt-icon wnt-icon-mobile"></span> '
-											. '<span class="wnt-visuallyhidden">Mobile phone:</span> '
-											. $agent['mobile_phone'] . '</li>';
-									}
-
-									if (strlen($agent['email_address']) > 0) {
-										echo '<li><span class="wnt-icon wnt-icon-mail"></span> '
-											. '<span class="wnt-visuallyhidden">Contact:</span> '
-											. '<a href="' . $contactLink . '">'
-											. $agent['first_name'] . ' ' . $agent['last_name']
-											. '</a></li>';
-									}
-
-									if (strlen($agent['address_1']) > 0) {
-										echo '<li><span class="wnt-icon wnt-icon-location"></span> '
-											. '<span class="wnt-visuallyhidden">Address:</span> '
-											. $agent['address_1'] . ' ' . $agent['address_2']
-											. '<br />'
-											. $agent['city'] . ', ' . $agent['state'] . ' '
-											. $agent ['zip_code'];
-									}
-
-								?>
-
-							</ul>
-
-							<div class="wolfnet_aoActions wolfnet_clearfix">
+						<div class="wolfnet_aoFooter">
+							<div class="wolfnet_aoActions">
 								<div class="wolfnet_aoAction">
 									<a class="wnt-btn wnt-btn-secondary"
 									 href="<?php echo $agentLink; ?>">View Profile</a>
@@ -221,7 +242,6 @@ if (!function_exists('paginate')) {
 									</div>
 								<?php } ?>
 							</div>
-
 						</div>
 
 					</div>
