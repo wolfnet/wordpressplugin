5d2d203ebc841993bf99e97aa10b80f44a336321
John Coleman <thenewcoke@gmail.com>
2016-05-23 20:39:58 +0100

    fix: Agent/Office nav JS error
    @22002714

--- src/template/agentPagesNav.php
+++ src/template/agentPagesNav.php
@@ -63,10 +63,8 @@ $officesLink = $linkBase . $postHash;
 
 	jQuery(function ($) {
 
-		var $officesWidget = $('#<?php echo $instance_id; ?>');
-
 		// Search field
-		var $searchForm = $officesWidget.find('.wolfnet_agentSearch');
+		var $searchForm = $('.wolfnet_agentOfficeNav .wolfnet_agentSearch');
 		var $criteria = $searchForm.find('.wolfnet_agentCriteria');
 		$criteria.css({ cursor: 'text' });
 		$criteria.click(function () {
