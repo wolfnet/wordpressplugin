5eb78405e37106ee2fa84daeab9e59d72efde211
andrew.baumgart <andrew.baumgart@wolfnet.com>
2017-01-20 13:12:50 -0600

    fix: Use php sanitizer in lieu of mistakenly used Wordpress Codex function

--- public/css/wolfnet.theme.custom.php
+++ public/css/wolfnet.theme.custom.php
@@ -10,11 +10,11 @@
 	$userOptions = array();
 
 	if (!empty($_REQUEST['colors'])) {
-		$userOptions['colors'] = explode(',', sanitize_text_field($_REQUEST['colors']));
+		$userOptions['colors'] = explode(',', FILTER_SANITIZE_NUMBER_FLOAT($_REQUEST['colors']));
 	}
 
 	if (!empty($_REQUEST['opacity'])) {
-		$userOptions['opacity'] = sanitize_text_field($_REQUEST['opacity']);
+		$userOptions['opacity'] = FILTER_SANITIZE_NUMBER_FLOAT($_REQUEST['opacity']);
 	}
 
 	$args = array_merge($styleDefaults, $userOptions);
