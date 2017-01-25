261c85777b8ef8518501dc1671f7fee899a15aeb
Jon Lipp <jon.lipp@wolfnet.com>
2017-01-25 16:20:41 -0600

    Removed comments from file

--- src/template/agentPagesAgentBrief.php
+++ src/template/agentPagesAgentBrief.php
@@ -25,13 +25,6 @@
 <div class="wolfnet_aoItem">
 
 	<div class="wolfnet_aoBody">
-
-
-		<?php 
-		//Strip out the extra commas and periods in the agentLink
-		//$agentLink = preg_replace("/\./", "", $agentLink);
-		//$agentLink = preg_replace("/,/", "", $agentLink);
-		?>
 		
 		<a href="<?php echo $agentLink; ?>">
 			<div class="wolfnet_aoImage"
