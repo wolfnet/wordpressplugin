04c2af0c6a720d26d1fac7556ff902ba6b6e6d35
andrew.baumgart <andrew.baumgart@wolfnet.com>
2017-06-02 09:42:33 -0500

    chore: Adding a TODO

--- src/template/quickSearchOptions.php
+++ src/template/quickSearchOptions.php
@@ -20,6 +20,7 @@
  *                Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+// TODO: Disable "Routing" select if Smartsearch is true (logic is specific to Quick Search)
 ?>
 
 <?php
