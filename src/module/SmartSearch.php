d6351084cba3e1e3f5c7b5080a713bf9e3982ce1
andrew.baumgart <andrew.baumgart@wolfnet.com>
2017-08-01 09:05:47 -0500

    style: Whitespace

--- src/module/SmartSearch.php
+++ src/module/SmartSearch.php
@@ -125,61 +125,61 @@ class Wolfnet_Module_SmartSearch
 			$multiMarket = false;
 		}
 
-        if (is_wp_error($productKey)) {
-            return $this->plugin->getWpError($productKey);
-        }
+		if (is_wp_error($productKey)) {
+			return $this->plugin->getWpError($productKey);
+		}
 
-        // Get data
-        $prices = $this->plugin->data->getPrices($productKey);
-        $beds = $this->plugin->data->getBeds();
-        $baths = $this->plugin->data->getBaths();
-        $formAction = $this->plugin->data->getBaseUrl($productKey);
+		// Get data
+		$prices = $this->plugin->data->getPrices($productKey);
+		$beds = $this->plugin->data->getBeds();
+		$baths = $this->plugin->data->getBaths();
+		$formAction = $this->plugin->data->getBaseUrl($productKey);
 
-        if (is_wp_error($prices)) {
-            return $this->plugin->getWpError($prices);
-        }
+		if (is_wp_error($prices)) {
+			return $this->plugin->getWpError($prices);
+		}
 
-        if (is_wp_error($beds)) {
-            return $this->plugin->getWpError($beds);
-        }
+		if (is_wp_error($beds)) {
+			return $this->plugin->getWpError($beds);
+		}
 
-        if (is_wp_error($baths)) {
-            return $this->plugin->getWpError($baths);
-        }
+		if (is_wp_error($baths)) {
+			return $this->plugin->getWpError($baths);
+		}
 
-        if (is_wp_error($formAction)) {
-            return $this->plugin->getWpError($formAction);
-        }
+		if (is_wp_error($formAction)) {
+			return $this->plugin->getWpError($formAction);
+		}
 
-        $vars = array(
-            'instance_id'  => str_replace('.', '', uniqid('wolfnet_quickSearch_')),
-            'siteUrl'      => site_url(),
-            'markets'      => $markets,
-            'multiMarket'  => $multiMarket,
-            'prices'       => $prices,
-            'beds'         => $beds,
-            'baths'        => $baths,
-            'formAction'   => $formAction,
+		$vars = array(
+			'instance_id'  => str_replace('.', '', uniqid('wolfnet_quickSearch_')),
+			'siteUrl'      => site_url(),
+			'markets'      => $markets,
+			'multiMarket'  => $multiMarket,
+			'prices'       => $prices,
+			'beds'         => $beds,
+			'baths'        => $baths,
+			'formAction'   => $formAction,
 		);
 
-        $args = $this->plugin->convertDataType(array_merge($criteria, $vars));
+		$args = $this->plugin->convertDataType(array_merge($criteria, $vars));
 
-        // Instantiate SmartSearch Service
-        $smartSearchService = $this->plugin->ioc->get(
-            'Wolfnet_Service_SmartSearchService',
-            array(
-                'key' => $this->plugin->keyService->getDefault(),
-                'url' => $this->plugin->url
-            )
-        );
+		// Instantiate SmartSearch Service
+		$smartSearchService = $this->plugin->ioc->get(
+			'Wolfnet_Service_SmartSearchService',
+			array(
+				'key' => $this->plugin->keyService->getDefault(),
+				'url' => $this->plugin->url
+			)
+		);
 
-        $args['smartSearchFields'] = json_encode($smartSearchService->getFields());
-        $args['smartSearchFieldMap'] = json_encode($smartSearchService->getFieldMap());
-        $args['smartSearchPlaceholder'] = $smartSearchService->getPlaceholder($criteria['isCanada']);
-        $args['componentId'] = uniqid('-');
+		$args['smartSearchFields'] = json_encode($smartSearchService->getFields());
+		$args['smartSearchFieldMap'] = json_encode($smartSearchService->getFieldMap());
+		$args['smartSearchPlaceholder'] = $smartSearchService->getPlaceholder($criteria['isCanada']);
+		$args['componentId'] = uniqid('-');
 
-        return $this->plugin->views->smartSearchView($args);
-    }
+		return $this->plugin->views->smartSearchView($args);
+	}
 
 
     /**
